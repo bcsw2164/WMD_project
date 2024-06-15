644114234b37dfa6b90f118ada65e374f0b06594
bcsw2164 <126849531+bcsw2164@users.noreply.github.com>
2024-06-16 05:24:54 +0900

    about에 텍스트 애니메이션 적용 완료

--- about.site/style/about.js
+++ about.site/style/about.js
@@ -1,4 +1,19 @@
-document.querySelectorAll('.marquee').forEach((marquee) => {
-  const content = marquee.innerHTML;
-  marquee.innerHTML += content; // 내용이 반복되도록 설정
+let scrollElement1 = document.querySelector('.scroll-1');
+let scrollElement2 = document.querySelector('.scroll-2');
+
+window.addEventListener('scroll', function () {
+  let value = window.scrollY;
+  console.log('scrollY', value);
+
+  if (value > 850) {
+    scrollElement1.style.animation = 'slide 1s ease-out forwards';
+  } else {
+    scrollElement1.style.animation = 'disappear 1s ease-out forwards';
+  }
+
+  if (value > 1100) {
+    scrollElement2.style.animation = 'slide 1s ease-out forwards';
+  } else {
+    scrollElement2.style.animation = 'disappear 1s ease-out forwards';
+  }
 });
