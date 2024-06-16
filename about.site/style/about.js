5210fbf2e2d77b721220e5524e5690a2a7c80905
bcsw2164 <126849531+bcsw2164@users.noreply.github.com>
2024-06-16 19:33:18 +0900

    about 에 vontent-1 사이즈 조절

--- about.site/style/about.js
+++ about.site/style/about.js
@@ -5,13 +5,13 @@ window.addEventListener('scroll', function () {
   let value = window.scrollY;
   console.log('scrollY', value);
 
-  if (value > 500) {
+  if (value > 600) {
     scrollElement1.style.animation = 'slide 1s ease-out forwards';
   } else {
     scrollElement1.style.animation = 'disappear 1s ease-out forwards';
   }
 
-  if (value > 700) {
+  if (value > 800) {
     scrollElement2.style.animation = 'slide 1s ease-out forwards';
   } else {
     scrollElement2.style.animation = 'disappear 1s ease-out forwards';
