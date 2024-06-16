cc6c2ec72af88e0ca5be3b6caa6d5707f7fb4419
bcsw2164 <126849531+bcsw2164@users.noreply.github.com>
2024-06-16 18:41:43 +0900

    99% 완료

--- about.site/style/about.js
+++ about.site/style/about.js
@@ -5,13 +5,13 @@ window.addEventListener('scroll', function () {
   let value = window.scrollY;
   console.log('scrollY', value);
 
-  if (value > 850) {
+  if (value > 500) {
     scrollElement1.style.animation = 'slide 1s ease-out forwards';
   } else {
     scrollElement1.style.animation = 'disappear 1s ease-out forwards';
   }
 
-  if (value > 1100) {
+  if (value > 700) {
     scrollElement2.style.animation = 'slide 1s ease-out forwards';
   } else {
     scrollElement2.style.animation = 'disappear 1s ease-out forwards';
